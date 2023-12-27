74dc0fb63fad3aa4b3bbb4e007e3a5d6a0f9144d
Miles Angels <milesmarieangels@gmail.com>
2023-12-26 16:28:05 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from './components/Navbar.jsx'
+import Main from './components/Main.jsx'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Main />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import reactLogo from "../assets/react.svg"
+
+export default function Navbar() {
+    return (
+        <nav className="navbar">
+            <img src={reactLogo} alt="React Logo"/>
+            <h3>React Facts</h3>
+            <h4>React Course - Project 1</h4>
+        </nav>
+    )
+}
